d0b579b19dcb22b5ab4bd85fb1ee6c459b7d6ad1
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-12-27 20:25:50 -0300

    bomb

--- src/services/CurrencyQuotes.ts
+++ src/services/CurrencyQuotes.ts
@@ -1,6 +1,6 @@
-import {bus} from "@/events/currencyQuoteEventEmitter";
-import {get} from "@/server/HttpClient";
-import type {CurrencyQuotes} from "@/types/CurrencyQuotes/CurrencyQuotes";
+import { bus } from "@/events/currencyQuoteEventEmitter";
+import { get } from "@/server/HttpClient";
+import type { CurrencyQuotes } from "@/types/CurrencyQuotes/CurrencyQuotes";
 
 /**
  * @description Handles the request to get the list of currency quotes.
@@ -9,18 +9,18 @@ import type {CurrencyQuotes} from "@/types/CurrencyQuotes/CurrencyQuotes";
  * @throws {Error} If the request to the proxy fails
  */
 export async function listCurrencyQuotes(): Promise<CurrencyQuotes | undefined> {
-	try {
-		const response = await get("/api/latest.json");
+    try {
+        const response = await get("/api/latest");
 
-		if (!response.ok) throw new Error(await response.json());
+        if (!response.ok) throw new Error(await response.json());
 
-		const quotes: CurrencyQuotes = await response.json();
+        const quotes: CurrencyQuotes = await response.json();
 
-		bus.emit("getCurrencyQuotes", {quotes});
+        bus.emit("getCurrencyQuotes", { quotes });
 
-		return quotes;
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
+        return quotes;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
